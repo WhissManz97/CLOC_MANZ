90edb56f029c2402c22315249f75ddce07d509a6
WhissManz97 <jlmanzano@titaniumsolutions.com.mx>
2021-01-11 17:44:53 -0600

    Ruta del archivo a leer

---
+++ ContadorLineasPalabras.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.StringTokenizer;
+import java.io.StreamTokenizer;
+
+public class ContadorLineasPalabras {
+
+    static int importTotales = 0;
+    static int lineasTotales = 0;
+    static int tareasTotales = 0;
+    
+    public static void main(String[] args) {
+       
+        // OBJETO ARCHIVO CON LA RUTA DEL ARCHIVO A LEER
+        File archivo = new File("/Users/Sonatadev/Documents/WhissJava/Contador/ContadorLineasPalabras.java");
+ 
